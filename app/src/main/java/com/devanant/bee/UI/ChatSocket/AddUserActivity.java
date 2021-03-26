49cc702d88a5ce2da0994b1bcbd06d172ef7481b
darpan1107 <mittal11darpan@gmail.com>
2021-03-26 15:43:22 +0530

    Chat using socketio

---
+++ app/src/main/java/com/devanant/bee/UI/ChatSocket/AddUserActivity.java
@@ -0,0 +1,61 @@
+package com.devanant.bee.UI.ChatSocket;
+
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.devanant.bee.R;
+
+public class AddUserActivity extends AppCompatActivity {
+
+    private Button setNickName;
+    private EditText userNickName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user);
+
+        userNickName = findViewById(R.id.userNickName);
+        setNickName = findViewById(R.id.setNickName);
+
+
+        userNickName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0) {
+                    setNickName.setEnabled(true);
+                   // Log.i(MainActivity.TAG, "onTextChanged: ABLED");
+                } else {
+                  //  Log.i(MainActivity.TAG, "onTextChanged: DISABLED");
+                    setNickName.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+
+        setNickName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddUserActivity.this, MainActivity.class);
+                intent.putExtra("username", userNickName.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
